0517c300219d87af2a17ce28f2da90b229c35d88
Khanh <uefa.euro30@gmail.com>
2021-08-23 14:01:31 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,116 @@
+// b2
+// let a = parseInt(prompt("Nhập n"))
+// let b = 1;
+// for (let i =1; i <= a; i++){
+//     b*=i;
+// }
+// console.log(b)
+//b3
+//     // hinh1
+// {
+//     for (let j = 1; j <= 5; j++) {
+//         for (let i = 1; i <= j; i++) {
+//             document.write("*")
+//         }
+//         document.write("<br>")
+//     }
+// }
+//     // hinh2
+// {
+//     document.write("<br>");
+//     for (let j = 5; j >= 1; j--) {
+//         for (let i = 1; i <= j; i++) {
+//             document.write("*")
+//         }
+//         document.write("<br>")
+//     }
+// }
+//     // hinh3
+// {
+//     let count = 1;
+//     while (count <= 4)
+//         for (let j = 4; j >= 1; j--) {
+//             for (let i = 1; i <= j; i++) {
+//                 document.write(`\xa0\xa0`)
+//             }
+//             for (let i = 1; i <= count; i++) {
+//                 document.write("*")
+//             }
+//             count++;
+//             document.write("<br>")
+//         }
+//     for (let i = 1; i <= 5; i++) {
+//         document.write("*")
+//     }
+//     document.write("<br>")
+//     document.write("<br>")
+// }
+//     // hinh4
+// {
+//     for (let i = 1; i <= 5; i++) {
+//         document.write("*");
+//     }
+//     document.write("<br>")
+//     let count1 = 4;
+//     while (count1 >= 1) {
+//         for (let j = 1; j <= 4; j++) {
+//             for (let i = 1; i <= j; i++) {
+//                 document.write(`\xa0\xa0`)
+//             }
+//             for (let i = 1; i <= count1; i++) {
+//                 document.write("*")
+//             }
+//             count1--;
+//             document.write("<br>")
+//         }
+//     }
+// }
+
+// b4
+// for (let i = 1; i <= 21; i++) {
+//     document.write("*")
+// }
+// document.write("<br>")
+// for (let i = 1 ; i <= 5; i++){
+//     for (let i = 1; i < 21; i++) {
+//         if(i == 1 || i == 20){
+//             document.write("*")
+//         }
+//         document.write(`\xa0\xa0`)
+//     }
+//     document.write("<br>")
+// }
+// for (let i = 1; i <= 21; i++) {
+//     document.write("*")
+// }
+
+// b6
+// for(let i = 1; i<= 4;i++){
+//     for (let j = 1;j<= 5;j++){
+//         if(i == 1 || i ==3){
+//             if(j==1 || j ==5){
+//                 document.write("\xa0\xa0")
+//             }
+//             else{
+//                 document.write("\xa0")
+//                 document.write("*")
+//             }
+//         }
+//         else if(i==4){
+//             if(j==5){
+//                 document.write("\xa0")
+//                 document.write("\xa0")
+//                 document.write("*")
+//             }
+//             else{
+//                 document.write("\xa0")
+//             }
+//         }
+//         else{
+//             document.write("*")
+//             document.write("\xa0")
+//         }
+//     }
+//     document.write("<br>")
+// }
+
